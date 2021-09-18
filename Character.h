cf3c6ac97b19b90ef1ede043fcb8a6ab0be4bf70
Andy Hotzler <ahotzler@gmail.com>
2021-09-17 22:11:30 -0500

    created Character.cpp and Character.h files to clean up and centralize code

---
+++ Character.h
@@ -0,0 +1,30 @@
+#include "raylib.h"
+#include "raymath.h"
+
+class Character
+{
+public:
+    Character();
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight); // function prototype
+    void tick(float deltaTime);
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    float width{};
+    float height{};
+
+
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+};
--- main.cpp
+++ main.cpp
@@ -1,84 +1,8 @@
 #include "raylib.h"
 #include "raymath.h"
-
-class Character
-{
-public:
-    Vector2 getWorldPos() { return worldPos; }
-    void setScreenPos(int winWidth, int winHeight); // function prototype
-    void tick(float deltaTime);
-
-private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-
-    // 1 : facing right, -1 : facing left
-    float rightLeft{1.f};
-
-    // animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrames{6};
-    const float updateTime{1.f / 12.f};
-    const float speed{4.f};
-};
-
-// adding Character::function name is "fully qualifying the function name"
-// :: scope resolution operator
-void Character::setScreenPos(int winWidth, int winHeight)
-{
-    screenPos = {
-        screenPos.x = (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f), 
-        screenPos.y = (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)
-    };
-}
-
-void Character::tick(float deltaTime)
-{
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A)) 
-        direction.x -= 1.0;
-    if (IsKeyDown(KEY_D)) 
-        direction.x += 1.0;
-    if (IsKeyDown(KEY_W)) 
-        direction.y -= 1.0;
-    if (IsKeyDown(KEY_S)) 
-        direction.y += 1.0;
-
-    if (Vector2Length(direction) != 0.0)
-    {
-        // set worldPos = worldPos + direction
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-        texture = run;
-    }
-    else
-    {
-        texture = idle;
-    }
-
-    // Update Animation Frames
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames)
-            frame = 0;
-    }
-
-    // Draw the character
-    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
+#include "Character.h"
 
 
-
-// Start of Main function
 int main()
 {
     const int windowWidth{384};
